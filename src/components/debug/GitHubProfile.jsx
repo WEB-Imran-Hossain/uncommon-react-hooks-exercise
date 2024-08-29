ae083d28961c9aec3ed19915c1ad450e3cf8b4a1
WEB-Imran-Hossain <webimran2021@gmail.com>
2024-08-29 23:56:30 +0600

    Github data is working perfectly from Github profile

--- src/components/debug/GitHubProfile.jsx
+++ src/components/debug/GitHubProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import useGitHub from './useGitHub'
 
 const GitHubProfile = () => {
--- src/components/debug/useGitHub.js
+++ src/components/debug/useGitHub.js
@@ -1,29 +1,30 @@
 import { useState, useEffect } from "react";
 
-
-
 const useGitHub = (username) => {
-    const [user, setUser]=useState(null)
-    const [error, setError]=useState(null)
-    const [loading, setLoading]=useState(false)
-    
-    useEffect(()=>{
-const fetchData=async()=>{
-setLoading(true)
-try {
-    const response=await fetch(`https://api.github.com/${username}`)
-    const responseData=await response.json()
-    console.log(responseData);
-    
-    
-} catch (error) {
-    setError(error)
-}
-}
-fetchData()
-    },[username])
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  return {user, loading, error}
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(`https://api.github.com/users/${username}`);
+        if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+        const responseData = await response.json();
+        setUser(responseData); // Set user data here
+        setLoading(false)
+      } catch (error) {
+        setError(error.message);
+        setLoading(false)
+      }
+    };
+    fetchData();
+  }, [username]);
 
-export default useGitHub
+  return { user, loading, error };
+};
+
+export default useGitHub;
